c0f1be1602e3241d2cc63333a2d7ccbd0513247c
Sébastien Mandaba <seb.mandaba@gmail.com>
2017-08-24 16:23:07 +0200

    db sql added

--- main.js
+++ main.js
@@ -2,11 +2,28 @@ const express = require('express');
 const mustache = require('mustache');
 const fs = require('fs');
 const basicAuth = require('express-basic-auth');
+const Client = require('mariasql');
 let app = express();
 let bodyParser = require('body-parser');
 let eventName;
 let eventLocation;
 let events = [];
+
+
+var c = new Client({
+    host: '127.0.0.1',
+    user: 'php',
+    password: 'toor',
+    db: 'eventfinder'
+  });
+
+  c.query('SELECT * FROM events', function(err, rows) {
+    if (err)
+      throw err;
+    console.dir(rows);
+  });
+  
+  c.end();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -36,13 +53,25 @@ app.post('/event/add', function (req, res) {
     // console.log(req.body.name);
     eventName = req.body.name;
     eventLocation = req.body.location;
+
+
+    var prep = c.prepare('INSERT INTO events(name, location, hour, category, description, organisator) VALUES (name, :location, :hour, :category, :description, :organisator);');
+    
+    c.query(prep({ name:"abc", location:"abc", hour:"abc", category:"abc", description:"abc", organisator:"abc" }), function(err, rows) {
+      if (err)
+        throw err;
+    });
+    
+    c.end();
+
+
     events.push({
         ev: {
             name: eventName,
             location: eventLocation
         }
     });
-    console.log(events);
+    // console.log(events);
 });
 
 app.engine("html", function (path, options, callback) {
