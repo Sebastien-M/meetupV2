5c8aae5efd4b2831d61e2a7672f71aba7ac9654f
cristophol <thomas.cristophol@gmail.com>
2017-09-07 17:20:54 +0200

    front from modif

--- public/js/form.js
+++ public/js/form.js
@@ -1,89 +1,73 @@
-window.alert = function () { };
-var defaultCSS = document.getElementById('bootstrap-css');
+"use strict";
 
-function changeCSS(css) {
-    if (css) $('head > link').filter(':first').replaceWith('<link rel="stylesheet" href="' + css + '" type="text/css" />');
-    else $('head > link').filter(':first').replaceWith(defaultCSS);
-}
-$(document).ready(function () {
-    var iframe_height = parseInt($('html').height());
-});
+// let title = document.getElementById("title").value;
+// let orga = document.getElementById("orga").value;
+// let cat = document.getElementById("cat").value;
+// let loca = document.getElementById("location").value;
+// let date = document.getElementById("date").value;
+// let text = document.getElementById("text").value;
 
 
 
-$(document).ready(function () {
-    //Initialize tooltips
-    $('.nav-tabs > li a[title]').tooltip();
-
-    //Wizard
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-
-        var $target = $(e.target);
-
-        if ($target.parent().hasClass('disabled')) {
-            return false;
-        }
-    });
-
-    $(".next-step").click(function (e) {
-
-        var $active = $('.wizard .nav-tabs li.active');
-        $active.next().removeClass('disabled');
-        nextTab($active);
-
-    });
-    $(".prev-step").click(function (e) {
-
-        var $active = $('.wizard .nav-tabs li.active');
-        prevTab($active);
-
-    });
-});
-
-function nextTab(elem) {
-    $(elem).next().find('a[data-toggle="tab"]').click();
-}
-
-function prevTab(elem) {
-    $(elem).prev().find('a[data-toggle="tab"]').click();
-}
+let step1 = document.getElementById("step1")
+let step2 = document.getElementById("step2")
+let step3 = document.getElementById("step3")
+let send1 = document.getElementById("send1")
+let send2 = document.getElementById("send2")
+let finalsend = document.getElementById("finalsend")
 
 
-document.querySelector("#endForm").addEventListener("click", function (e) {
-    let title = document.getElementById("title").value;
-    let orga = document.getElementById("orga").value;
-    let cat = document.getElementById("cat").value;
-    let loca = document.getElementById("location").value;
-    let date = document.getElementById("date").value;
-    let text = document.getElementById("text").value;
-    if (title === "" ||
-        orga === "" ||
-        cat === "" ||
-        loca === "" ||
-        date === "" ||
-        text === "") {
-        console.log("All filds must be completed");
-        document.querySelector("#statut").textContent = "Error";
-        document.querySelector("#result").textContent = "All filds must be completed!";
 
-    } else {
-        console.log("OK");
-        document.querySelector("#statut").textContent = "Complete";
-        document.querySelector("#result").textContent = "You have successfully completed all steps.";
+send1.addEventListener('click', function(e) {
+    step1.style.display = 'none';
+    step2.style.display = 'block';
+    e.preventDefault();
 
-        //SENDING AJAX REQUEST TO THE PAGE /EVENT/ADD HANDLED BY EXPRESS SERVER
-        let params = "name=" + title + "&location=" + loca + "&orga=" + orga + "&cat=" + cat + "&date=" + date + "&desc=" + text;
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', '/event/add', true);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    console.log("Form sent");
-                }
-            }
-        }
-        xhr.send(params);
-        
-    }
 })
+send2.addEventListener('click', function(e) {
+    step2.style.display = 'none';
+    step3.style.display = 'block';
+    e.preventDefault();
+
+})
+
+
+
+
+
+
+
+
+
+// document.querySelector("#endForm").addEventListener("click", function(e) {
+//     if (title === "" ||
+//         orga === "" ||
+//         cat === "" ||
+//         loca === "" ||
+//         date === "" ||
+//         text === "") {
+//         console.log("All filds must be completed");
+//         document.querySelector("#statut").textContent = "Error";
+//         document.querySelector("#result").textContent = "All filds must be completed!";
+
+//     } else {
+//         console.log("OK");
+//         document.querySelector("#statut").textContent = "Complete";
+//         document.querySelector("#result").textContent = "You have successfully completed all steps.";
+
+//         //SENDING AJAX REQUEST TO THE PAGE /EVENT/ADD HANDLED BY EXPRESS SERVER
+//         let params = "name=" + title + "&location=" + loca + "&orga=" + orga + "&cat=" + cat + "&date=" + date + "&desc=" + text;
+//         let xhr = new XMLHttpRequest();
+//         xhr.open('POST', '/event/add', true);
+//         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//         xhr.onreadystatechange = function() {
+//             if (xhr.readyState === XMLHttpRequest.DONE) {
+//                 if (xhr.status === 200) {
+//                     console.log("Form sent");
+//                 }
+//             }
+//         }
+//         xhr.send(params);
+
+//     }
+// })
