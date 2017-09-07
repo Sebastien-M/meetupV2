ca7a32f7a9733b8d68c1ff63688911a3644aba72
cristophol <thomas.cristophol@gmail.com>
2017-09-07 17:36:44 +0200

    form

--- public/js/form.js
+++ public/js/form.js
@@ -1,11 +1,11 @@
 "use strict";
 
-// let title = document.getElementById("title").value;
-// let orga = document.getElementById("orga").value;
-// let cat = document.getElementById("cat").value;
-// let loca = document.getElementById("location").value;
-// let date = document.getElementById("date").value;
-// let text = document.getElementById("text").value;
+let title = document.getElementById("title").value;
+let orga = document.getElementById("orga").value;
+let cat = document.getElementById("cat").value;
+let loca = document.getElementById("location").value;
+let date = document.getElementById("date").value;
+let text = document.getElementById("text").value;
 
 
 
@@ -39,35 +39,35 @@ send2.addEventListener('click', function(e) {
 
 
 
-// document.querySelector("#endForm").addEventListener("click", function(e) {
-//     if (title === "" ||
-//         orga === "" ||
-//         cat === "" ||
-//         loca === "" ||
-//         date === "" ||
-//         text === "") {
-//         console.log("All filds must be completed");
-//         document.querySelector("#statut").textContent = "Error";
-//         document.querySelector("#result").textContent = "All filds must be completed!";
+document.querySelector("#endForm").addEventListener("click", function(e) {
+    if (title === "" ||
+        orga === "" ||
+        cat === "" ||
+        loca === "" ||
+        date === "" ||
+        text === "") {
+        console.log("All filds must be completed");
+        document.querySelector("#statut").textContent = "Error";
+        document.querySelector("#result").textContent = "All filds must be completed!";
 
-//     } else {
-//         console.log("OK");
-//         document.querySelector("#statut").textContent = "Complete";
-//         document.querySelector("#result").textContent = "You have successfully completed all steps.";
+    } else {
+        console.log("OK");
+        document.querySelector("#statut").textContent = "Complete";
+        document.querySelector("#result").textContent = "You have successfully completed all steps.";
 
-//         //SENDING AJAX REQUEST TO THE PAGE /EVENT/ADD HANDLED BY EXPRESS SERVER
-//         let params = "name=" + title + "&location=" + loca + "&orga=" + orga + "&cat=" + cat + "&date=" + date + "&desc=" + text;
-//         let xhr = new XMLHttpRequest();
-//         xhr.open('POST', '/event/add', true);
-//         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//         xhr.onreadystatechange = function() {
-//             if (xhr.readyState === XMLHttpRequest.DONE) {
-//                 if (xhr.status === 200) {
-//                     console.log("Form sent");
-//                 }
-//             }
-//         }
-//         xhr.send(params);
+        //SENDING AJAX REQUEST TO THE PAGE /EVENT/ADD HANDLED BY EXPRESS SERVER
+        let params = "name=" + title + "&location=" + loca + "&orga=" + orga + "&cat=" + cat + "&date=" + date + "&desc=" + text;
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', '/event/add', true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    console.log("Form sent");
+                }
+            }
+        }
+        xhr.send(params);
 
-//     }
-// })
+    }
+})
