1d9120a29c135068d8d3b3fe20996910691c29ad
Sébastien Mandaba <seb.mandaba@gmail.com>
2017-09-12 09:56:02 +0200

    User register working

--- public/js/register.js
+++ public/js/register.js
@@ -1,3 +1,88 @@
+// let buton1 = document.querySelector("#send1");
+// let buton2 = document.querySelector("#send2");
+// buton1.addEventListener("click", (e)=>{e.preventDefault();})
+// buton2.addEventListener("click", (e)=>{e.preventDefault();})
+
+
+
+let step1 = document.getElementById("step1")
+let step2 = document.getElementById("step2")
+let step3 = document.getElementById("step3")
+let send1 = document.getElementById("send1")
+let send2 = document.getElementById("send2")
+let finalsend = document.getElementById("finalsend")
+let titre1 = document.getElementById('titre1');
+let titre2 = document.getElementById('titre2');
+let titre3 = document.getElementById('titre3');
+let back1 = document.getElementById('back1');
+let back2 = document.getElementById('back2');
+let back3 = document.getElementById('back3');
+
+
+
+send1.addEventListener('click', function(e) {
+    step1.style.opacity = 0;
+    step1.style.display = 'none';
+    titre1.style.display = "none";
+    titre2.style.display = "block";
+    step2.style.display = 'block';
+
+    setTimeout(function() {
+
+        step2.style.opacity = 1;
+    }, 10);
+    e.preventDefault();
+
+})
+send2.addEventListener('click', function(e) {
+    step2.style.opacity = 0;
+    step2.style.display = 'none';
+    titre2.style.display = "none";
+    titre3.style.display = "block";
+    step3.style.display = 'block';
+
+    setTimeout(function() {
+
+        step3.style.opacity = 1;
+    }, 10);
+    e.preventDefault();
+
+})
+back2.addEventListener('click', function(e) {
+    titre2.style.display = "none";
+    step2.style.display = 'none';
+    step1.style.display = 'block';
+    titre1.style.display = "block";
+
+
+    setTimeout(function() {
+
+        step1.style.opacity = 1;
+    }, 10);
+
+    e.preventDefault();
+
+})
+back3.addEventListener('click', function(e) {
+    titre3.style.display = "none";
+    step3.style.display = 'none';
+    step2.style.display = 'block';
+    titre2.style.display = "block";
+
+    setTimeout(function() {
+
+        step2.style.opacity = 1;
+    }, 10);
+
+    e.preventDefault();
+
+})
+
+
+
+
+
+
 document.querySelector("#sendForm").addEventListener("click", function(e) {
     e.preventDefault();
     let nom = document.getElementById("nom").value;
